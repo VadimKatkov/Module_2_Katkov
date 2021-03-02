e3d965ad25c02e3ff67cb02812f041fe79a50f2a
VadimKatkov <62391897+VadimKatkov@users.noreply.github.com>
2021-03-02 08:31:58 +0200

    checking assumptions using boxplot

--- Module_2.py
+++ Module_2.py
@@ -18,7 +18,7 @@ for col in ['Medu', 'Fedu', 'traveltime', 'studytime', 'failures']:
     list(map(lambda item: print(col, item) if item > 4 else None, students[col]))
 
 #только одно значение в поле Fedu не отвечает условию. Предполагаем, что это ошибка. Заменим его на 4.
-students.Fedu.replace(40,4)
+students.Fedu = students.Fedu.replace(40,4)
 
 #тоже сделаем для признаков famrel, freetime, goout, health. Но проверим на не превышение 5
 for col in ['famrel','freetime','goout','health']:
@@ -35,7 +35,8 @@ students = students[students.age <=20]
 
 cols = ["schoolsup","famsup","paid","activities","nursery","higher","internet","romantic","score",'Medu',
          "Fedu", "traveltime", "studytime", "failures","famrel","freetime","goout","health"]
-students.replace(to_replace={"schoolsup":{'yes': 1,'no':0},
+students.replace(to_replace={"Pstatus":{'T':1,'A':0},
+                             "schoolsup":{'yes': 1,'no':0},
                              "famsup":{'yes': 1,'no':0},
                              "paid":{'yes': 1,'no':0},
                              "activity":{'yes': 1,'no':0},
@@ -45,12 +46,27 @@ students.replace(to_replace={"schoolsup":{'yes': 1,'no':0},
                              "romantic":{'yes': 1,'no':0}}, inplace=True)
 
 print(students.schoolsup.value_counts())
-print(students[cols].corr())
 
-for col in students.columns:
-    pct_missing = students[col].isna().mean()
+# описание аналитик Medu & Fedu содержит категорию  0-нет. Делаем предположение, что в 21-ом века начальное образование
+# есть у всех то наблюдения должны принимать значения от 1до 4. Помиотрим сколько таких запмсей.
+print('Образование матери:\n',students.Medu.value_counts())
+print('Образование отца:\n',students.Fedu.value_counts())
+
+# значений 0 не так много, но попробуем их заменить. Оценим как это раполагается в группах мать-отец
+print(students.pivot_table(index=['Medu'], columns=['Fedu'], values=['school'], aggfunc='count'))
+
+students['Medu'] = students.apply(lambda row: row['Fedu'] if row['Medu'] == 0 or pd.isnull(row['Medu']) else row['Medu'], axis=1)
+students['Fedu'] = students.apply(lambda row: row['Medu'] if row['Fedu'] == 0 or pd.isnull(row['Fedu']) else row['Fedu'], axis=1)
+print(students.pivot_table(index=['Medu'], columns=['Fedu'], values=['school'], aggfunc='count'))
+
+# тепловая карта показывает, что наибольшую корреляцию score показывает со следующими категориями:
+# age, higher, romantic, Medu, studityme, failures, goout, address Выделим их в отдельный датасет и опять построим heatmap
+students_short = students[['score', 'age', 'higher', 'romantic', 'Medu', 'studytime', 'failures', 'goout', 'address']]
+#пока не удалось понять назначение поля granular. Это дубликат поля studytime с коэффициентом -3.
+
+# оценим пропуски в новом наборе
+for col in students_short.columns:
+    pct_missing = students_short[col].isna().mean()
     print(f'{col} - {pct_missing :.1%}')
 
-print(students.groupby(['school', 'higher'])['score'].mean())
-print(students.groupby(['school', 'higher'])['score'].count())
 
